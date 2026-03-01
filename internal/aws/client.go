cf49f23c44e931893d879bb0a41873d7abd8d326
ppiankov <103106369+ppiankov@users.noreply.github.com>
2026-03-01 20:23:50 +0800

    feat: add AWS CloudWatch Logs scanner

--- internal/aws/client.go
+++ internal/aws/client.go
@@ -1,1 +1,14 @@
 package aws
+
+import (
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/cloudwatch"
+	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs"
+)
+
+// NewCloudWatchScannerFromConfig creates a CloudWatchScanner using real AWS SDK clients.
+func NewCloudWatchScannerFromConfig(cfg aws.Config, lookbackDays int) *CloudWatchScanner {
+	logsClient := cloudwatchlogs.NewFromConfig(cfg)
+	cwClient := cloudwatch.NewFromConfig(cfg)
+	return NewCloudWatchScanner(logsClient, logsClient, logsClient, cwClient, lookbackDays)
+}
